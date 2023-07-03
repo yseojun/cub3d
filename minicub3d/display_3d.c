6951099c8cf748da3414d12730d862788f0b3187
“rolee” <rolee@student.42seoul.kr>
2023-07-03 18:55:38 +0900

    set color background

--- minicub3d/display_3d.c
+++ minicub3d/display_3d.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   display_3d.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seojyang <seojyang@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rolee <rolee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:25:08 by seojyang          #+#    #+#             */
-/*   Updated: 2023/07/03 16:33:49 by seojyang         ###   ########.fr       */
+/*   Updated: 2023/07/03 18:54:52 by rolee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "info.h"
+
+static void display_background(t_info *info);
 
 void	display_3d(t_info *info)
 {
@@ -22,6 +24,7 @@ void	display_3d(t_info *info)
 	info->frame.img = mlx_new_image(info->mlx, WIDTH, HEIGHT);
 	info->frame.addr = mlx_get_data_addr(info->frame.img, &info->frame.bits, \
 		&info->frame.line_length, &info->frame.endian);
+	display_background(info);
 	while (x < WIDTH)
 	{
 		ray = set_ray(info, x);
@@ -30,4 +33,32 @@ void	display_3d(t_info *info)
 		x++;
 	}
 	mlx_put_image_to_window(info->mlx, info->win, info->frame.img, 0, 0);
+}
+
+int encode_rgb(int color[3])
+{
+	return (color[0] << 16 | color[1] << 8 | color[2]);
+}
+
+static void display_background(t_info *info)
+{
+	int i;
+	int j;
+	int	*tx;
+
+	tx = (int *)info->frame.addr;
+	i = 0;
+	while (i < HEIGHT)
+	{
+		j = 0;
+		while (j < WIDTH)
+		{
+			if (i < HEIGHT / 2)
+				tx[i * WIDTH + j] = encode_rgb(info->c_color);
+			else
+				tx[i * WIDTH + j] = encode_rgb(info->f_color);
+			j++;
+		}
+		i++;
+	}
 }
