f3bdc4b3aa73165f2867706b55a3ebb54e39bfe8
Seojun Yang <seojyang@c5r5s3.42seoul.kr>
2023-07-04 17:32:11 +0900

    add minimap_user

--- minicub3d/display_minimap.c
+++ minicub3d/display_minimap.c
@@ -6,66 +6,19 @@
 /*   By: seojyang <seojyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:18:07 by seojyang          #+#    #+#             */
-/*   Updated: 2023/07/04 17:01:38 by seojyang         ###   ########.fr       */
+/*   Updated: 2023/07/04 17:31:39 by seojyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "info.h"
-
-// void	display_map(t_info *info)
-// {
-// 	int		i;
-// 	int		j;
-	
-// 	i = -1;
-// 	while (info->map[++i])
-// 	{
-// 		j = -1;
-// 		while (info->map[i][++j])
-// 		{
-// 			if (info->map[i][j] != ' ')
-// 				mlx_put_image_to_window(info->mlx, info->win, \
-// 								info->so_texture, j * 64, i * 64);
-// 			if (info->map[i][j] == '1')
-// 				mlx_put_image_to_window(info->mlx, info->win, \
-// 								info->no_texture, j * 64, i * 64);
-// 			if (info->map[i][j] == 'P')
-// 				mlx_put_image_to_window(info->mlx, info->win, \
-// 								info->we_texture, j * 64, i * 64);
-// 		}
-// 	}
-// }
-
-// void	display_player(t_info *info)
-// {
-// 	int hit = 0;
-// 	double	nowX;
-// 	double	nowY;
-// 	double	distX = 0;
-// 	double	distY = 0;
-// 	t_draw dr;
-
-// 	nowX = info->player.posX;
-// 	nowY = info->player.posY;
-// 	while (hit == 0)
-// 	{
-// 		nowX += info->player.planeX;
-// 		distX += info->player.planeX;
-// 		nowY += info->player.planeY;
-// 		distY += info->player.planeY;
-// 		if (info->map[(int)nowY][(int)nowX] == '1')
-// 			hit = 1;
-// 	}
-// 	dr = make_dr_info(info->player.posX * 64, info->player.posY * 64, nowX * 64, nowY * 64);
-// 	bresenham(info, dr);
-// }
-
 
 static char	get_map_inf(t_info *info, int mini_y, int mini_x)
 {
 	int	map_x;
 	int	map_y;
 
+	if (95 <= mini_x && mini_x <= 105 && 95 <= mini_y && mini_y <= 105)
+		return ('m');
 	map_y = floor(info->player.pos[Y] - ((double)5 / 100 * (100 - mini_y)));
 	map_x = floor(info->player.pos[X] - ((double)5 / 100 * (100 - mini_x)));
 	if (map_y < 0 || map_y >= 8 || map_x < 0 || map_x >= 10)
@@ -87,7 +40,7 @@ void	display_minimap(t_info *info)
 	{
 		mini_x = 0;
 		while (mini_x < 200)
-		{
+		{				
 			now = get_map_inf(info, mini_y, mini_x);
 			if (now == 0 || now == ' ')
 				mini[mini_y * 200 + mini_x] = 0xCCCCCC;
@@ -95,6 +48,8 @@ void	display_minimap(t_info *info)
 				mini[mini_y * 200 + mini_x] = 0xCCFFCC;
 			else if (now == '1')
 				mini[mini_y * 200 + mini_x] = 0x003300;
+			else if (now == 'm')
+				mini[mini_y * 200 + mini_x] = 0xFF0000;
 			mini_x++;
 		}
 		mini_y++;
